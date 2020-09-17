ebc6491a6cfa9af014e70e6ea167a0570911c478
sirtralala <thomas.doerflinger@gmx.net>
2020-09-17 14:57:22 +0200

    added search functionality, improved code readability

--- js/script.js
+++ js/script.js
@@ -1,41 +1,25 @@
-//////////////////////////////////// +++++++++ BORDERREQUEST USING FETCH +++++++++ //////////////////////////
-/* const status = response => {
-    if (response.status >= 200 && response.status < 300) {
-        return Promise.resolve(response);
-    }
-    return Promise.reject(new Error(response.statusText));
+//////////////////////////////////////////////////////////////////////////////////////
+// GLOBAL VARIABLES
+
+let home = document.querySelector('.header__title');
+let searchbar = document.querySelector('.searchbar');
+let search = document.querySelector('#search');
+let dropdown = document.querySelector('#region');
+let displayedCountries = document.querySelectorAll('.item');
+let button = document.querySelector('.btn-back');
+
+updateDisplayedCountries = () => {
+    displayedCountries = document.querySelectorAll('.item');
 }
-  
-const json = response => response.json();
-
-getBordersUsingFetch = async borders => {
-    let borderString = '';
-    borders.forEach ( border => {
-        borderString += `${border.toLowerCase()};`;
-    });
-    
-    //borderString = await fetch(`https://restcountries.eu/rest/v2/alpha?codes=${borderString}`)
-        //.then(status)    // note that the `status` function is actually **called** here, and that it **returns a promise***
-        //.then(json)      // likewise, the only difference here is that the `json` function here returns a promise that resolves with `data`
-        //.then(data => {  // ... which is why `data` shows up here as the first parameter to the anonymous function
-            //console.log('Request succeeded with JSON response', data);
-            //return data;
-        //})
-        //.catch(error => console.log('Request failed', error));
-
-    let response = await fetch(`https://restcountries.eu/rest/v2/alpha?codes=${borderString}`);
-    let json = await response.json();
-
-    console.log(json);
-    return json;
-} */
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//////////////////////////////////////////////////////////////////////////////////////
+// API REQUEST FUNCTIONS
 
 executeRequest = (region = null) => {
     let request = new XMLHttpRequest();
-    let url = region ? `https://restcountries.eu/rest/v2/region/${region}` : 'https://restcountries.eu/rest/v2/all';
+    let url = 'https://restcountries.eu/rest/v2/';
+    (region && region != 'all' && region != 'none') ? url += `region/${region}` : url +='all';
 
     request.open('GET', url);
     request.addEventListener('load', () => {
@@ -47,8 +31,10 @@ executeRequest = (region = null) => {
                 let html = generateHtmlMultipleCountries(item);
                 insertHtml(html);
             });
+            updateDisplayedCountries();
         } else {
-           console.warn(request.statusText, request.responseText);
+            console.warn(request.statusText, request.responseText);
+            document.querySelector('.content').innerHTML = `Error status ${request.status}: Something went wrong with the API request...`;
         }
     });
     request.send();
@@ -92,8 +78,16 @@ getBorders = async borders => {
     return borderData;
 }
 
+
+//////////////////////////////////////////////////////////////////////////////////////
+// DOM MANIPULATION
+
 clearCountries = () => {
     document.querySelector('.countries').innerHTML = '';
+}
+
+isDarkMode = () => {
+    return document.querySelector('body').classList.contains('body-darkmode');
 }
 
 insertHtml = html => {
@@ -104,26 +98,69 @@ insertCommas = number => {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-generateHtmlMultipleCountries = item => {
-    let darkmode = document.querySelector('body').classList.contains('body-darkmode') ? ' item-darkmode' : '';
-    return `<div class="item${darkmode}" id="${item.alpha3Code}" onclick="showDetails('${item.alpha3Code}')">
-                   <div class="item__flag">
-                      <img src="${item.flag}" alt="${item.name}" class="item__flag--img">
-                   </div>
-                   <h2 class="item__h2">${item.name}</h2>
-                   <p class="item__text">Population: <span class="item__text--data">${insertCommas(item.population)}</span></p>
-                   <p class="item__text">Region: <span class="item__text--data">${item.region}</span></p>
-                   <p class="item__text">Capital: <span class="item__text--data">${item.capital}</span></p>
-                </div>`;
-}
-
 getItemString = items => {
     let itemString = '';
     items.forEach (item => {
         itemString += `${item.name}, `;   
     });
     return itemString.slice(0, itemString.length - 2);
-}  
+}
+
+getBorderButtons = borderObject => {
+    let buttons = '';
+    for (let i = 0; i < borderObject.countries.length; i++) {
+        buttons += `<button class="item-single__details--button${isDarkMode() ? ' item-single__details--button-darkmode' : ''}" 
+                    onclick="showDetails('${borderObject.ids[i]}')">${borderObject.countries[i]}</button>`;
+    }
+    return buttons;
+}
+
+generateHtmlMultipleCountries = item => {
+    return `<div class="item${isDarkMode() ? ' item-darkmode' : ''}" id="${item.alpha3Code}" onclick="showDetails('${item.alpha3Code}')">
+                <div class="item__flag">
+                    <img src="${item.flag}" alt="${item.name}" class="item__flag--img">
+                </div>
+                <h2 class="item__h2">${item.name}</h2>
+                <p class="item__text">Population: <span class="item__text--data">${insertCommas(item.population)}</span></p>
+                <p class="item__text">Region: <span class="item__text--data">${item.region}</span></p>
+                <p class="item__text">Capital: <span class="item__text--data">${item.capital}</span></p>
+            </div>`;
+}
+
+generateHtmlCountryDetails = (item, borderObject) => {
+    return `<div class="item-single${isDarkMode() ? ' item-single-darkmode' : ''}" id="${item.alpha3Code}">
+                   <div class="item-single__flag">
+                      <img src="${item.flag}" alt="${item.name}" class="item-single__flag--img">
+                   </div>
+                   <div class="item-single__details">
+                      <h2 class="item-single__details--h2">${item.name}</h2>
+                      <div class="item-single__details--list">
+                         <div class="item-single__details--list-left">
+                            <p class="item-single__details--text">Native Name: <span class="item-single__details--text-data">${insertCommas(item.nativeName)}</span></p>
+                            <p class="item-single__details--text">Population: <span class="item-single__details--text-data">${insertCommas(item.population)}</span></p>
+                            <p class="item-single__details--text">Region: <span class="item-single__details--text-data">${item.region}</span></p>
+                            <p class="item-single__details--text">Sub region: <span class="item-single__details--text-data">${item.subregion}</span></p>
+                            <p class="item-single__details--text">Capital: <span class="item-single__details--text-data">${item.capital}</span></p>
+                         </div>
+                         <div class="item-single__details--list-right">
+                            <p class="item-single__details--text">Top Level Domain: <span class="item-single__details--text-data">${item.topLevelDomain}</span></p>
+                            <p class="item-single__details--text">Currencies: <span class="item-single__details--text-data">${getItemString(item.currencies)}</span></p>
+                            <p class="item-single__details--text">Languages: <span class="item-single__details--text-data">${getItemString(item.languages)}</span></p>
+                         </div>
+                      </div>
+                      <div class="item-single__details--border">
+                        <p class="item-single__details--border-margin">Border Countries:</p>
+                        <div class="item-single__details--border-buttons">
+                            ${typeof borderObject !== 'undefined' ? getBorderButtons(borderObject) : '<span class="item-single__details--text-data"> none</span>'}
+                        </div>
+                        </div>
+                   </div>
+                </div>`;
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+// TOGGLE FUNCTIONS
 
 removeClassLists = (items, className) => {
     items.forEach( item => {
@@ -183,17 +220,14 @@ toggleDarkMode = () => {
 }
 
 toggleSearchbar = () => {
-    let button = document.querySelector('.btn-back'),
-        searchbar = document.querySelector('.searchbar');
-    if (!button) searchbar.style.display = 'flex';
+    if (!document.querySelector('.btn-back')) searchbar.style.display = 'flex';
     else searchbar.style.display = 'none';
 }
 
 toggleBackButton = () => {
-    let button = document.querySelector('.btn-back');
     if (button) button.remove();
     else {
-        let buttonHtml = `<button class="btn-back${isDarkMode() ? ' btn-back-darkmode' : ''}" onclick="executeRequest()">
+        let buttonHtml = `<button class="btn-back${isDarkMode() ? ' btn-back-darkmode' : ''}" onclick="executeRequest(dropdown.value.toLowerCase())">
                             <svg class="btn-back__icon${isDarkMode() ? ' btn-back__icon-darkmode' : ''}">
                               <use xlink:href="./images/sprite.svg#icon-arrow-left"></use>
                             </svg>
@@ -203,50 +237,9 @@ toggleBackButton = () => {
     }
 }
 
-isDarkMode = () => {
-    return document.querySelector('body').classList.contains('body-darkmode');
-}
 
-getBorderButtons = borderObject => {
-    let buttons = '';
-    for (let i = 0; i < borderObject.countries.length; i++) {
-        buttons += `<button class="item-single__details--button${isDarkMode() ? ' item-single__details--button-darkmode' : ''}" 
-                    onclick="showDetails('${borderObject.ids[i]}')">${borderObject.countries[i]}</button>`;
-    }
-    return buttons;
-}
-
-generateHtmlCountryDetails = (item, borderObject) => {
-    let html = `<div class="item-single${isDarkMode() ? ' item-single-darkmode' : ''}" id="${item.alpha3Code}">
-                   <div class="item-single__flag">
-                      <img src="${item.flag}" alt="${item.name}" class="item-single__flag--img">
-                   </div>
-                   <div class="item-single__details">
-                      <h2 class="item-single__details--h2">${item.name}</h2>
-                      <div class="item-single__details--list">
-                         <div class="item-single__details--list-left">
-                            <p class="item-single__details--text">Native Name: <span class="item-single__details--text-data">${insertCommas(item.nativeName)}</span></p>
-                            <p class="item-single__details--text">Population: <span class="item-single__details--text-data">${insertCommas(item.population)}</span></p>
-                            <p class="item-single__details--text">Region: <span class="item-single__details--text-data">${item.region}</span></p>
-                            <p class="item-single__details--text">Sub region: <span class="item-single__details--text-data">${item.subregion}</span></p>
-                            <p class="item-single__details--text">Capital: <span class="item-single__details--text-data">${item.capital}</span></p>
-                         </div>
-                         <div class="item-single__details--list-right">
-                            <p class="item-single__details--text">Top Level Domain: <span class="item-single__details--text-data">${item.topLevelDomain}</span></p>
-                            <p class="item-single__details--text">Currencies: <span class="item-single__details--text-data">${getItemString(item.currencies)}</span></p>
-                            <p class="item-single__details--text">Languages: <span class="item-single__details--text-data">${getItemString(item.languages)}</span></p>
-                         </div>
-                      </div>
-                      <div class="item-single__details--border">
-                        <p class="item-single__details--border-margin">Border Countries:</p>
-                        <div class="item-single__details--border-buttons">
-                            ${typeof borderObject !== 'undefined' ? getBorderButtons(borderObject) : '<span class="item-single__details--text-data"> none</span>'}
-                        </div>
-                        </div>
-                   </div>
-                </div>`;
-    return html;
-}
+//////////////////////////////////////////////////////////////////////////////////////
+// USER INTERACTION FUNCTIONS
 
 showDetails = async id => {
     let country = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`)
@@ -259,99 +252,27 @@ showDetails = async id => {
     insertHtml(html);
 }
 
-let dropdown = document.querySelector('#region');
-clickHandler = () => {
-    if (dropdown.value != 'none') {
-        executeRequest(dropdown.value.toLowerCase());
-    }
-}
-dropdown.addEventListener('click', clickHandler);
-
-if ('ontouchstart' in window) {
-    dropdown.addEventListener('touchstart', () => {
-        let touchHndl = () => {
-            // call the clickHandler actually
-            clickHandler();
-            // remove the touchend handler after perform
-            this.removeEventListener('touchend', touchHndl)
-        }
-        // attach a handler for touch end when you are in touchstart event
-        this.addEventListener('touchend', touchHndl);
+searchCountry = (value, displayedCountries) => {
+    clearCountries();
+    displayedCountries.forEach( country => {
+        let countryName = country.childNodes[3].innerText.toLowerCase();
+        if (countryName.includes(value)) document.querySelector('.countries').appendChild(country);
     });
 }
 
+search.addEventListener('keyup', () => {
+    if (search.value != undefined) searchCountry(search.value.toLowerCase(), displayedCountries);
+});
 
-let home = document.querySelector('.header__title');
+search.addEventListener('search', () => {
+    if (search.value.length == 0) executeRequest(dropdown.value.toLowerCase());
+});
+
+dropdown.addEventListener('click', () => {
+    if (dropdown.value != 'none') executeRequest(dropdown.value.toLowerCase());
+});
+
 home.addEventListener('click', () => {
     executeRequest();
     dropdown.value = 'none';
 });
-
-
-
-
-
-
-
-/* let dropdown = document.querySelector('#region');
-dropdown.addEventListener('click', () => {
-    let selection = document.querySelector('#region').value;
-    if (selection != 'none') {
-        executeRequest(selection.toLowerCase());
-    }
-}); */
-
-/* generateHtmlSingleCountry = item => {
-    let darkmode = document.querySelector('body').classList.contains('body-darkmode') ? ' item-darkmode' : '';
-    let html = `<div class="item${darkmode}" id="${item.alpha3Code}">
-                   <div class="item__flag">
-                      <img src="${item.flag}" alt="${item.name}" class="item__flag--img">
-                   </div>
-                   <h2 class="item__h2">${item.name}</h2>
-                   <p class="item__text">Native Name: <span class="item__text--data">${insertCommas(item.nativeName)}</span></p>
-                   <p class="item__text">Population: <span class="item__text--data">${insertCommas(item.population)}</span></p>
-                   <p class="item__text">Region: <span class="item__text--data">${item.region}</span></p>
-                   <p class="item__text">Sub region: <span class="item__text--data">${item.subregion}</span></p>
-                   <p class="item__text">Capital: <span class="item__text--data">${item.capital}</span></p>
-
-                   <p class="item__text">Top Level Domain: <span class="item__text--data">${item.topLevelDomain}</span></p>
-                   <p class="item__text">Currencies: <span class="item__text--data">${getItemString(item.currencies)}</span></p>
-                   <p class="item__text">Languages: <span class="item__text--data">${getItemString(item.languages)}</span></p>
-
-                   <p class="item__text">Border Countries: <span class="item__text--data">${getBorders(item.borders)}</span></p>
-                </div>`;
-    return html;
-    // getBordersUsingFetch(item.borders)
-} */
-
-
-/* toggleDarkMode = () => {
-    let classNames = ['.header', '.header__switch--icon', '.searchbar__input', '.searchbar__select', '.attribution'];
-    let body = document.querySelector('body');
-    let items = document.querySelectorAll('.item');
-
-    if (body.classList.contains('body-darkmode')) {
-        body.classList.remove('body-darkmode');
-
-        for (let i = 0; i < classNames.length; i++) {
-            let element = document.querySelector(classNames[i]);
-            element.classList.remove(classNames[i] + '-darkmode');
-        }
-
-        items.forEach( item => {
-            item.classList.remove('item-darkmode');
-        });
-    }
-    else {
-        body.classList.add('body-darkmode');
-
-        for (let i = 0; i < classNames.length; i++) {
-            let element = document.querySelector(classNames[i]);
-            element.classList.add(classNames[i] + '-darkmode');
-        }
-
-        items.forEach( item => {
-            item.classList.add('item-darkmode');
-        });
-    }
-} */
